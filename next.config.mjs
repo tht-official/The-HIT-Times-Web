7e59068d62ec08db3e795cabd4957e7e2c80ffb2
Apurv Ajay Kumar <aakapurv@gmail.com>
2024-04-25 22:28:49 +0530

    Added protected api routes

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,18 @@ import createMDX from '@next/mdx'
 
 const nextConfig = {
     pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'avatars.githubusercontent.com',
+            },
+        ],
+    },
 };
 
 const withMDX = createMDX({
