2ab86267c76fd472d88620861844fe527985f087
Apurv Ajay Kumar <aakapurv@gmail.com>
2024-06-26 15:54:33 +0530

    chore: Update layout and styles for better user experience

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,15 @@ export default function RootLayout({
   return (
     <SessionWrapper>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className + " bg-body"}>
+          {
+            // TODO: Add a header component here 
+          }
+          <div className="max-w-screen-2.5xl 2.5xl:mx-auto mx-4">{children}</div>
+          {
+            // TODO: Add a footer component here
+          }
+        </body>
       </html>
     </SessionWrapper>
   );
