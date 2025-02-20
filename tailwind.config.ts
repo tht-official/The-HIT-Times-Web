e2288e5950cd037879cc1ae9effb125d8566e0e6
Kingshuk Hazra <kingshukhazra5@gmail.com>
2025-02-20 22:58:49 +0530

    feat: add TSP form API and enhance TSP success page with calendar components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -45,6 +45,50 @@ const config: Config = {
       animation: {
         typing: "typing 2s steps(20), blink .7s infinite"
       },
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          // DEFAULT: "hsl(var(--primary))",
+          // foreground: "hsl(var(--primary-foreground))",
+          DEFAULT: "#0D9488",
+          foreground: "#ffffff",
+        },
+        secondary: {
+          // DEFAULT: "hsl(var(--secondary))",
+          // foreground: "hsl(var(--secondary-foreground))",
+          DEFAULT: "#FB923C",
+          foreground: "#ffffff",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
       
 
 
